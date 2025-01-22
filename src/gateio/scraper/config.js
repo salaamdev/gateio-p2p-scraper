8647bb5ae200cdfe62cb4436f5a7f1c8cb99e07f
Salaam <abdisalamxhassan@gmail.com>
2025-01-22 16:56:15 +0300

    add: create configuration file for Gate.io scraper settings

---
+++ src/gateio/scraper/config.js
@@ -0,0 +1,7 @@
+// scraper/config.js
+
+// Any scraper-related configuration can live here.
+module.exports = {
+    URL: "https://www.gate.io/p2p/buy/USDT-KES",
+    chromeExecutablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+};
