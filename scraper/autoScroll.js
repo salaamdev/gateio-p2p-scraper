35b914219e8c3a36df9aba75681519be2e2f4832
Abdisalam Hassan <abdisalamhassan@gmail.com>
2025-08-09 17:15:16 +0300

    fix(scraper): Improve autoScroll robustness

--- scraper/autoScroll.js
+++ scraper/autoScroll.js
@@ -9,19 +9,22 @@ const {errorLog} = require('./logger');
 async function autoScroll (page) {
     try {
         await page.evaluate(async () => {
+            const start = Date.now();
+            const MAX_MS = 15000;
             await new Promise((resolve) => {
-                let totalHeight = 0;
-                const distance = 100;
-                const timer = setInterval(() => {
-                    const scrollHeight = document.body.scrollHeight;
+                let lastHeight = document.body.scrollHeight;
+                const distance = 200;
+                const interval = setInterval(() => {
                     window.scrollBy(0, distance);
-                    totalHeight += distance;
-
-                    if (totalHeight >= scrollHeight - window.innerHeight) {
-                        clearInterval(timer);
+                    const newHeight = document.body.scrollHeight;
+                    const timeExceeded = Date.now() - start > MAX_MS;
+                    const atBottom = window.innerHeight + window.scrollY >= document.body.scrollHeight - 2;
+                    if (newHeight === lastHeight || timeExceeded || atBottom) {
+                        clearInterval(interval);
                         resolve();
                     }
-                }, 100);
+                    lastHeight = newHeight;
+                }, 150);
             });
         });
     } catch (err) {
